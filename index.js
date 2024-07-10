bc52089f53994bcf901f359f190101b7437f6eda
saikrishna321 <saikrishna321@yahoo.com>
2024-07-10 20:56:38 +0530

    fix: should make a release

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //check pr title
 // Should not release - 1.0.5 released - chore
 // Should not release - test - No release
-// Should not release - docs
+// Should not release - docs - No release
+// Should release - fix
